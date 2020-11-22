e4aa38ac50ff2dea920d7d281d391518f8a7c1a7
joansaj <joannsaj@gmail.com>
2020-11-22 16:28:17 +0300

    added forms

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,9 +1,19 @@
 from django import forms
-from .models import Profile
+from .models import Profile, Image, Comment
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
         
-    
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('image','name', 'caption')
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment')
