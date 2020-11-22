eac5cba0321f3785d4403a6b8aacc357459c9cbd
joansaj <joannsaj@gmail.com>
2020-11-22 16:09:09 +0300

    added methods to image model

--- instagram/models.py
+++ instagram/models.py
@@ -11,19 +11,46 @@ class Profile(models.Model):
     bio = models.TextField(null=True)
 
     def __str__(self):
-        return self.user
+        return self.name
 
     def save_profile(self):
         self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+    
     
 
 class Image(models.Model):
     image = CloudinaryField('image')
     image_name = models.CharField(max_length=30)
     image_caption = models.TextField(null=True)
-    profile_key = models.ForeignKey('Profile' ,on_delete=models.CASCADE)
-    likes = models.BooleanField(null= True)
-    comments = models.TextField(null=True)
+    profile_key = models.ForeignKey('Profile' ,on_delete=models.CASCADE, related_name='posts')
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-pk"]
+
+    @property
+    def get_comments(self):
+        return self.comments.all()
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return f'{self.profile_key.name} Image'
 
 # class Comment(db.Model):
 #     __tablename__ = 'comments'
